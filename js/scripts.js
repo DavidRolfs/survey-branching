5cc13fe519562b31cdd55edbd5eaf709702d2e1c
david rolfs and guy anderson <david@>
2017-04-27 14:28:17 -0700

    add alert to notify blank input field

--- js/scripts.js
+++ js/scripts.js
@@ -5,31 +5,27 @@ $(document).ready(function(){
     var personalityInput = $("#personality").val();
     var ageInput = $("input:radio[name=age]:checked").val()
 
-  if(genderInput==="male"){
-    alert("you selected male")
-    if(personalityInput==="introvert" && ageInput=== "minor"||personalityInput==="extrovert" && ageInput=== "youngAdult"){
-      alert("color is red");
-    } else if(personalityInput==="introvert" && ageInput=== "youngAdult" || ageInput=== "overTheHill" ){
-      alert("color is blue");
+  if(ageInput){
+    if(genderInput==="male"){
+      if(personalityInput==="introvert" && ageInput=== "minor"||personalityInput==="extrovert" && ageInput=== "youngAdult"){
+        alert("color is red");
+      } else if(personalityInput==="introvert" && ageInput=== "youngAdult" || ageInput=== "overTheHill" ){
+        alert("color is blue");
+      } else{
+        alert("color is green")
+      }
     } else{
-      alert("color is green")
-    }
+        if(personalityInput==="introvert" && ageInput ==="youngAdult"){
+          alert("your color is yellow")
+        } else if(personalityInput==="extrovert" && ageInput=== "overTheHill"|| ageInput === "minor"){
+          alert("your color is purple")
+        } else{
+          alert("color is brown")
+        }
+      }
+  } else{
+    alert("select data")
   }
-
-
-  else{
-    alert("you selected female")
-    if(personalityInput==="introvert" && ageInput ==="youngAdult"){
-      alert("your color is yellow")
-    }
-    else if(personalityInput==="extrovert" && ageInput=== "overTheHill"|| ageInput === "minor"){
-      alert("your color is purple")
-    }
-    else{
-      alert("color is brown")
-    }
-  }
-
 
   })
 })
