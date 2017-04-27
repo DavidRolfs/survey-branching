414b3f9364bc408319e8d5aad18dee4fbe20a454
david rolfs and guy anderson <guy@>
2017-04-27 13:44:47 -0700

    create initial files, add submit button, store users input in variables

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("form#survey").submit(function(){
+    var genderInput = $("#gender").val();
+    var personalityInput = $("#personality").val();
+    var ageInput = $("input:radio[name=age]:checked").val()
+
+  
+
+
+
+  })
+})
